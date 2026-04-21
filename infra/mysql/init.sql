8983a7999912ef14f3f8a7fc902fcaed7b863a8c
jferminh <jfermin79@gmail.com>
2026-04-21 13:23:21 +0200

    fix: remove backticks from USE statement in init.sql

--- infra/mysql/init.sql
+++ infra/mysql/init.sql
@@ -1,7 +1,6 @@
 CREATE
 DATABASE  IF NOT EXISTS `resto` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE
-`resto`;
+USE resto;
 -- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
 -- Host: localhost    Database: resto
@@ -130,8 +129,7 @@ CREATE TABLE `status`
 -- JEU D'ESSAI INITIAL - BASE RESTO
 -- ==========================================
 
-USE
-`resto`;
+USE resto;
 
 -- 1. Insertion des statuts de commande
 -- Toujours insérer les nomenclatures en premier pour éviter les erreurs de clés étrangères
